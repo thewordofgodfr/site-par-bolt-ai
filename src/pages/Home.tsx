c4618fa66e8e067d9fdc2a55190929c222966592
thewordofgodfr <contact@thewordofgod.fr>
2025-10-12 11:33:08 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -158,7 +158,7 @@ export default function Home() {
           </div>
 
           {/* Citation fixe (cliquable) */}
-          <div className="text-center mt-12">
+          <div className="text-center mt-14">
             <button
               onClick={openJeremiah23}
               className="mx-auto block focus:outline-none"
