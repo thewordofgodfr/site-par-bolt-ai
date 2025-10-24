20dd66d8353181bf47c7e4eb652832640e0b6faa
thewordofgodfr <contact@thewordofgod.fr>
2025-10-24 11:10:27 +0200

    Add dark mode configuration to Tailwind CSS
    Enable dark mode support using the 'class' strategy.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // ⬅️ indispensable pour piloter le thème via la classe .dark
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
